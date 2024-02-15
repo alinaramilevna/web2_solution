6ff5634d8b1e906cf34b9a3085a1a28f5892e6c6
ПК <alina.kush2009@gmail.com>
2024-02-15 19:16:46 +0500

    3 задача

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
 @app.route('/training/<prof>')
-def index(prof):
+def training(prof):
     return render_template('training.html', prof=prof)
+
 
 @app.route('/list_prof/<type>')
 def crew_list(type):
-    pass
+    arr = 'инженер-исследователь, пилот, строитель, экзобиолог, врач, инженер по терраформированию, климатолог, ' \
+          'специалист по радиационной защите, астрогеолог, ' \
+          'гляциолог, инженер жизнеобеспечения, метеоролог, оператор марсохода, киберинженер, ' \
+          'штурман, пилот дронов'.split(', ')
+    return render_template('crew_list.html', list=arr, type=type)
 
 
 if __name__ == '__main__':
